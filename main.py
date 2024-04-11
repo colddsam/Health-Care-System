d42c7697a3bead0564f82642c9a1669acd61a459
colddsam <dassamratkumar772@gmail.com>
2024-04-11 15:50:19 +0530

    adding functionality for medium

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
-import os
 from python.gspread import GspreadOperation
 from datetime import datetime
 import secret as sc
-load_dotenv()
-
 SERVICE_ACCOUNT_FILE=sc.SERVICE_ACCOUNT_FILE
 SCOPES = sc.SCOPES
 SPREADSHEET_ID = sc.SPREADSHEET_ID
-
-# if __name__=='__main__':
-#     gspread = GspreadOperation(
-#         SERVICE_ACCOUNT_FILE=SERVICE_ACCOUNT_FILE, SCOPES=SCOPES, SPREADSHEET_ID=SPREADSHEET_ID)
-#     value = [datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), 20.10,10,30]
-#     res = gspread.append_data(value)
-#     res = gspread.show_data()
-#     print(type(res))
 
 
 app = FastAPI()
