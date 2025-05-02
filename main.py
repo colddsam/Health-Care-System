8ee7115db74206bdaed86732c3b4cd648ddee30b
colddsam <dassamratkumar772@gmail.com>
2025-05-02 21:25:47 +0530

    testing append

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ class Data(BaseModel):
     spo2: float
     temperature: float
     ECGSignal:float
+    PPG: list[float]
 
 class Device(BaseModel):
     value: float
@@ -104,18 +105,19 @@ async def assignPatient(doctorid:int,patientid:int,deviceid:int):
 async def append(_id: int, data: Data):
     try:
         data_dict = data.model_dump()
-        clientid = getDevice.getClientID(deviceid=_id)
-        stressLevel = ml.stressCalculation([data_dict['heart_rate']])
-        if(data_dict['spo2']<=0):
-            data_dict["spo2"]=-1
-        if((data_dict['heart_rate']>=200)or(data_dict['heart_rate']<=40)):
-            data_dict['heart_rate']=-1
-            stressLevel=-1
-        if(data_dict["temperature"]<=0):
-            data_dict['temperature']=-1
-        value = [datetime.now().strftime(
-            "%m/%d/%Y, %H:%M:%S"), data_dict['spo2'], data_dict['temperature'], data_dict['heart_rate'], data_dict['ECGSignal'],stressLevel]
-        res = gspread.appendData(_id=str(clientid), data=value)
+        print(data_dict)
+        # clientid = getDevice.getClientID(deviceid=_id)
+        # stressLevel = ml.stressCalculation([data_dict['heart_rate']])
+        # if(data_dict['spo2']<=0):
+        #     data_dict["spo2"]=-1
+        # if((data_dict['heart_rate']>=200)or(data_dict['heart_rate']<=40)):
+        #     data_dict['heart_rate']=-1
+        #     stressLevel=-1
+        # if(data_dict["temperature"]<=0):
+        #     data_dict['temperature']=-1
+        # value = [datetime.now().strftime(
+        #     "%m/%d/%Y, %H:%M:%S"), data_dict['spo2'], data_dict['temperature'], data_dict['heart_rate'], data_dict['ECGSignal'],stressLevel]
+        # res = gspread.appendData(_id=str(clientid), data=value)
         return {"report": res}
     except Exception as e:
         return {"report": "negative", "error": str(e)}
